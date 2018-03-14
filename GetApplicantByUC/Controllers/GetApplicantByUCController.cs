f9864620d0c5e38f72d3c442842b130d024b6be3
Kenneth McKenzieddfsdf <kenneth.mckenzie@myacuity.coms>
2018-03-14 08:59:35 -0400

    Add project files.

---
+++ GetApplicantByUC/Controllers/GetApplicantByUCController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GetApplicantByUC.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+
+namespace GetApplicantByUC.Controllers
+{
+    [Route("api/GetApplicantByUC")]
+    public class GetApplicantByUCController : Controller
+    {
+        private DatabaseContext _context;
+
+        public GetApplicantByUCController(DatabaseContext context)
+        {
+            _context = context;
+        }
+
+        // GET api/GetApplicantByUC
+        [HttpGet]
+        //public string Get([FromBody]Applicant applicant)
+        public string Get()
+        {
+            string strUniqueCode = "Get an applicant by specifying a unique code.";
+            return strUniqueCode;
+        }
+
+        // POST api/GetApplicantByUC
+        [HttpPost]
+        public string Post([FromBody]Applicant applicant)
+        {
+            string strApplicantData = GetApplicant(applicant.appUniqueCode);
+
+            return strApplicantData;
+        }
+
+
+        private string GetApplicant(string strUniqueCode)
+        {
+            string strApplicants = string.Empty;
+            using (_context)
+            {
+                var applicant = _context.Applicants.FirstOrDefault(c => c.appUniqueCode == strUniqueCode);
+                JsonSerializer serializer = new JsonSerializer();
+                var json = JsonConvert.SerializeObject(applicant);
+                strApplicants = json;
+            }
+
+            return strApplicants;
+        }
+    }
+
+}
